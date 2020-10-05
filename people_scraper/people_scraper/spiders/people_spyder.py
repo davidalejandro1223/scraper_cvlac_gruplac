bd2d2230cacac12d58b0fd3516c141d3cf04f02e
David Aparicio <davidalejandro1223@hotmail.com>
2020-10-04 19:13:13 -0500

    Funcionalidad de extraccion de campos requeridos completos

--- people_scraper/people_scraper/spiders/people_spyder.py
+++ people_scraper/people_scraper/spiders/people_spyder.py
@@ -14,4 +14,25 @@ class PeopleScrapy(scrapy.Spider):
     def parse(self, response):
         nombre = response.xpath('/html[1]/body[1]/div[1]/div[3]/table[1]/tr[2]/td[1]/table[1]/tr[3]/td[2]/text()').get()
         nombre = unicodedata.normalize('NFKD', nombre.strip())
+        titulo = response.xpath('/html[1]/body[1]/div[1]/div[3]/table[1]/tr[3]/td[1]/table[1]/tr[2]/td[2]/text()').get()
+        titulo = unicodedata.normalize('NFKD', titulo.strip())
+        cvlac = response.url
+        orcid = response.xpath('/html[1]/body[1]/div[1]/div[3]/table[1]/tr[2]/td[1]/table[1]/tr[7]/td[1]/a[1]/@href').get()
+        if orcid is not None:
+             orcid = unicodedata.normalize('NFKD',orcid.strip())
+        investigador = response.xpath('/html[1]/body[1]/div[1]/div[3]/table[1]/tr[2]/td[1]/table[1]/tr[2]/td[2]/text()').get()
+        investigador = unicodedata.normalize('NFKD', investigador.strip())
+        areas = response.xpath('/html[1]/body[1]/div[1]/div[3]/table[1]/tr[6]/td[1]/table[1]')
+        areas_actuacion = []
+        
+        for area in areas:
+            actuacion = area.xpath('tr/td[1]/li[1]/text()').getall()
+            areas_actuacion = map(lambda x: unicodedata.normalize('NFKD',x.strip()),actuacion)
+        
+
+
         print(nombre)
+        print(titulo)
+        print(orcid)
+        print(investigador)
+        print(list(areas_actuacion))
