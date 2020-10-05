4308e441e3edbac47f65a21dd8297b86d18186ca
David Aparicio <davidalejandro1223@hotmail.com>
2020-10-04 22:58:26 -0500

    Mejora en titulo de cvlac y tipo investigador

--- people_scraper/people_scraper/spiders/people_spyder.py
+++ people_scraper/people_scraper/spiders/people_spyder.py
@@ -14,19 +14,17 @@ class PeopleScrapy(scrapy.Spider):
     def parse(self, response):
         
         tabla_intro = response.xpath('/html[1]/body[1]/div[1]/div[3]/table[1]/tr[2]/td[1]/table/tr')
+        investigador = None
         for item in tabla_intro:
             nom_col = item.xpath('td[1]/text()').get()
             nom_orcid = item.xpath('td[1]/a/text()').get()
             print(nom_col)
             if nom_col=='Nombre':
                 nombre = item.xpath('td[2]/text()').get()
-                nombre = unicodedata.normalize('NFKD', nombre.strip())
+                nombre = unicodedata.normalize('NFKD', nombre.strip()).title()
             if nom_col=='Categoría':
-                try:
-                    investigador = item.xpath('td[2]/text()').get()
-                    investigador = unicodedata.normalize('NFKD', investigador.strip())
-                except:
-                    investigador = None
+                investigador = item.xpath('td[2]/text()').get().strip().splitlines()
+                investigador = unicodedata.normalize('NFKD', investigador[0])
             if nom_orcid is not None and nom_orcid=='Código ORCID':
                 orcid = item.xpath('td[1]/a/@href').get()
                 orcid = unicodedata.normalize('NFKD',orcid.strip())
@@ -34,7 +32,9 @@ class PeopleScrapy(scrapy.Spider):
                 orcid = None
 
         
-        titulo = response.xpath('/html[1]/body[1]/div[1]/div[3]/table[1]/tr[3]/td[1]/table[1]/tr[2]/td[2]/text()').get()
+        titulo = response.xpath('/html[1]/body[1]/div[1]/div[3]/table[1]/tr[3]/td[1]/table[1]/tr[2]/td[2]/b/text()').get()
+        area_titulo = response.xpath('/html[1]/body[1]/div[1]/div[3]/table[1]/tr[3]/td[1]/table[1]/tr[2]/td[2]/text()').getall()
+        titulo = titulo.strip() +"-"+ area_titulo[1].strip()
         titulo = unicodedata.normalize('NFKD', titulo)
         
         cvlac = response.url       
@@ -45,7 +45,7 @@ class PeopleScrapy(scrapy.Spider):
             actuacion = area.xpath('tr/td[1]/li[1]/text()').getall()
             areas_actuacion = map(lambda x: unicodedata.normalize('NFKD',x.strip()),actuacion)
         
-
+        
         yield {
             'nombre':nombre,
             'cvlac':cvlac,
